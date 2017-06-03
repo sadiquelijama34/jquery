9811350e9fea4ee499937a89c03345294f29f50b
sadiquealijama34 <sadiquealijama34@gmail.com>
2017-06-03 11:20:35 +0300

    added everthing

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("This is a header.");
+    alert("I told you,THIS IS A HEADER!");
+  });
+
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
+
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+});
